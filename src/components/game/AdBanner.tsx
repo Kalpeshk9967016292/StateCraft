0460e8eba8f0710c49091c415b779dc5ba18f3b2
Kalpesh Kalekar <kalpeshk9967016292@gmail.com>
2025-08-15 01:29:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/game/AdBanner.tsx
+++ src/components/game/AdBanner.tsx
@@ -9,25 +9,16 @@ declare global {
 }
 
 const AdBanner = () => {
-  const insRef = useRef<HTMLModElement>(null);
-  const adPushed = useRef(false);
-
   useEffect(() => {
-    // We only want to push the ad once.
-    // If the ref is there and we haven't pushed before, push the ad.
-    if (insRef.current && !adPushed.current) {
-        try {
-          (window.adsbygoogle = window.adsbygoogle || []).push({});
-          adPushed.current = true; // Mark as pushed
-        } catch (err) {
-          console.error(err);
-        }
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {
+      console.error(err);
     }
   }, []);
 
   return (
     <ins
-      ref={insRef}
       className="adsbygoogle"
       style={{ display: 'block' }}
       data-ad-client="ca-pub-4648414963251970"
